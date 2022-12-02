4665013f0e19af9a9043e21c74ad575329a9fcb4
Tyson McNulty <tyson.mcnulty@gmail.com>
2022-12-01 20:25:20 -0500

    Add day 1 solutions

---
+++ src/day01/__init__.py
@@ -0,0 +1,55 @@
+def load(data_file: str):
+    elves = []
+    with open(data_file) as calorie_data:
+        calorie_counts = []
+        for line in calorie_data:
+            if line.isspace():
+                elves.append(Elf([FoodItem(ct) for ct in calorie_counts]))
+                calorie_counts = []
+            else:
+                calorie_counts.append(int(line.strip()))
+
+    if len(calorie_counts) > 0:
+        elves.append(Elf([FoodItem(ct) for ct in calorie_counts]))
+
+    return elves
+
+
+class FoodItem:
+    def __init__(self, calorie_count: int):
+        self.calorie_count = calorie_count
+
+    def __eq__(self, other):
+        return self.calorie_count == other.calorie_count
+
+    def __str__(self):
+        return f"{self.calorie_count}"
+
+    def __repr__(self):
+        return f"FoodItem({str(self)})"
+
+
+class Elf:
+    def __init__(self, food_items: list[FoodItem]):
+        self.food_items = food_items
+
+    def __eq__(self, other):
+        return self.food_items == other.food_items
+
+    def __str__(self):
+        return f"{self.food_items}"
+
+    def __repr__(self):
+        return f"Elf({str(self)})"
+
+
+def elf_calorie_count(elf: Elf):
+    return sum([f.calorie_count for f in elf.food_items])
+
+
+def get_max_elf_calorie_count(elves: list[Elf]):
+    return max([elf_calorie_count(elf) for elf in elves])
+
+
+def get_top_three_elf_calorie_counts(elves: list[Elf]):
+    return sorted([elf_calorie_count(elf) for elf in elves], reverse=True)[:3]
---
+++ tests/test_day01.py
@@ -0,0 +1,65 @@
+import unittest
+
+from src.day01 import (
+    Elf,
+    FoodItem,
+    get_max_elf_calorie_count,
+    get_top_three_elf_calorie_counts,
+    load,
+)
+
+
+class Day01Tests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.example = load("tests/resources/day01/example.txt")
+        cls.input = load("src/day01/input.txt")
+
+    def test_load_example(self):
+        self.assertEqual(
+            self.example,
+            [
+                Elf(
+                    food_items=[
+                        FoodItem(calorie_count=1000),
+                        FoodItem(calorie_count=2000),
+                        FoodItem(calorie_count=3000),
+                    ]
+                ),
+                Elf(
+                    food_items=[
+                        FoodItem(calorie_count=4000),
+                    ]
+                ),
+                Elf(
+                    food_items=[
+                        FoodItem(calorie_count=5000),
+                        FoodItem(calorie_count=6000),
+                    ]
+                ),
+                Elf(
+                    food_items=[
+                        FoodItem(calorie_count=7000),
+                        FoodItem(calorie_count=8000),
+                        FoodItem(calorie_count=9000),
+                    ]
+                ),
+                Elf(
+                    food_items=[
+                        FoodItem(calorie_count=10000),
+                    ]
+                ),
+            ],
+        )
+
+    def test_get_max_calorie_count(self):
+        self.assertEqual(get_max_elf_calorie_count(self.example), 24000)
+
+    def test_get_top_three_elf_calorie_counts(self):
+        self.assertEqual(
+            get_top_three_elf_calorie_counts(self.example), [24000, 11000, 10000]
+        )
+
+    def test_solutions(self):
+        self.assertEqual(get_max_elf_calorie_count(self.input), 68292)
+        self.assertEqual(sum(get_top_three_elf_calorie_counts(self.input)), 203203)
