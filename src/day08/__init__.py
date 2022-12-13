383bd417dcbc5fcc64ef9cc690b9b9e3f009fdb7
Tyson McNulty <tyson.mcnulty@gmail.com>
2022-12-12 20:12:24 -0500

    Add day 8 part 2 solution

--- src/day08/__init__.py
+++ src/day08/__init__.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field, InitVar
 from enum import Enum
-from functools import cache
+from functools import cached_property
 from itertools import zip_longest
+from math import prod
 from typing import Optional, Iterator
 
 
@@ -30,21 +31,32 @@ def get_visible_trees(trees: Iterator[Tree]) -> set[Tree]:
     return visible_trees
 
 
+def takeuntil(predicate, iterable):
+    found = False
+    for x in iterable:
+        if found:
+            break
+        if predicate(x):
+            found = True
+
+        yield x
+
+
 @dataclass(frozen=True)
 class Forest:
-    from_trees: InitVar[list[list[Tree]]]
-    trees: list[list[Tree]] = field(init=False)
+    trees: InitVar[list[list[Tree]]]
+    rows: list[list[Tree]] = field(init=False)
 
-    def __post_init__(self, from_trees: list[list[Tree]]):
+    def __post_init__(self, trees: list[list[Tree]]):
         object.__setattr__(
             self,
-            "trees",
+            "rows",
             [
                 [
-                    Tree(from_trees[i][j].height, location=(i, j))
-                    for j in range(0, len(from_trees[i]))
+                    Tree(trees[i][j].height, location=(i, j))
+                    for j in range(0, len(trees[i]))
                 ]
-                for i in range(0, len(from_trees))
+                for i in range(0, len(trees))
             ],
         )
 
@@ -54,19 +66,43 @@ class Forest:
         visible_trees = set()
 
         if Direction.NORTH in from_directions:
-            for column in zip_longest(*self.trees):
+            for column in self.columns:
                 visible_trees |= get_visible_trees(c for c in column if c is not None)
         if Direction.SOUTH in from_directions:
-            for column in zip_longest(*reversed(self.trees)):
+            for column in zip_longest(*reversed(self.rows)):
                 visible_trees |= get_visible_trees(c for c in column if c is not None)
         if Direction.EAST in from_directions:
-            for row in self.trees:
+            for row in self.rows:
                 visible_trees |= get_visible_trees(iter(row))
         if Direction.WEST in from_directions:
-            for row in self.trees:
+            for row in self.rows:
                 visible_trees |= get_visible_trees(reversed(row))
 
         return visible_trees
+
+    @cached_property
+    def columns(self):
+        return [*zip_longest(*self.rows)]
+
+    def get_scenic_score_by_location(self, location: tuple[int, int]):
+        r, c = location
+        trees_to_the_north = self.columns[c][r::-1][1:]
+        trees_to_the_south = self.columns[c][r:][1:]
+        trees_to_the_east = self.rows[r][c:][1:]
+        trees_to_the_west = self.rows[r][c::-1][1:]
+        return prod(
+            map(
+                lambda arr: len(
+                    [*takeuntil(lambda t: t.height >= self.rows[r][c].height, arr)]
+                ),
+                [
+                    trees_to_the_north,
+                    trees_to_the_south,
+                    trees_to_the_east,
+                    trees_to_the_west,
+                ],
+            )
+        )
 
 
 def parse(data: list[str]):
--- tests/test_day08.py
+++ tests/test_day08.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import chain
 from pathlib import Path
 
 from src import load_data
@@ -30,7 +31,7 @@ class Day08Tests(unittest.TestCase):
         )
 
     def test_get_tree_in_forest(self):
-        self.assertEqual(Tree(3, location=(0, 0)), self.example.trees[0][0])
+        self.assertEqual(Tree(3, location=(0, 0)), self.example.rows[0][0])
 
     def test_get_visible_trees_from_north_and_south(self):
         forest = Forest(
@@ -43,8 +44,14 @@ class Day08Tests(unittest.TestCase):
             ]
         )
         self.assertEqual(
-            {Tree(3, location=(0, 0)), Tree(6, location=(2, 0)), Tree(3, location=(4, 0))},
-            forest.get_visible_trees(from_directions={Direction.NORTH, Direction.SOUTH}),
+            {
+                Tree(3, location=(0, 0)),
+                Tree(6, location=(2, 0)),
+                Tree(3, location=(4, 0)),
+            },
+            forest.get_visible_trees(
+                from_directions={Direction.NORTH, Direction.SOUTH}
+            ),
         )
 
     def test_get_visible_trees_from_east_and_west(self):
@@ -54,14 +61,38 @@ class Day08Tests(unittest.TestCase):
             ]
         )
         self.assertEqual(
-            {Tree(3, location=(0, 0)), Tree(7, location=(0, 3)), Tree(3, location=(0, 4))},
+            {
+                Tree(3, location=(0, 0)),
+                Tree(7, location=(0, 3)),
+                Tree(3, location=(0, 4)),
+            },
             forest.get_visible_trees(from_directions={Direction.EAST, Direction.WEST}),
         )
 
     def test_example_get_visible_trees(self):
         self.assertEqual(21, len(self.example.get_visible_trees()))
 
+    def test_example_scenic_score(self):
+        self.assertEqual(4, self.example.get_scenic_score_by_location((1, 2)))
+        self.assertEqual(8, self.example.get_scenic_score_by_location((3, 2)))
+
+        self.assertEqual(
+            8,
+            max(
+                self.example.get_scenic_score_by_location(t.location)
+                for t in chain.from_iterable(self.example.rows)
+            ),
+        )
+
     def test_solutions(self):
         self.assertEqual(1546, len(self.input.get_visible_trees()))
-
-
+        self.assertEqual(
+            (519064, Tree(height=8, location=(46, 7))),
+            max(
+                (
+                    (self.input.get_scenic_score_by_location(t.location), t)
+                    for t in chain.from_iterable(self.input.rows)
+                ),
+                key=lambda t: t[0],
+            ),
+        )
